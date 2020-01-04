83857296495e833bb314d89d1f92a1497bc95a31
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-01-04 15:17:26 -0500

    Roll my own navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "react-quill/dist/quill.snow.css";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 import Routes from "./Routes";
 
 // document.title = "Logd Note-Taking App";
@@ -9,12 +9,29 @@ const App: React.FC = () => {
 
   return (
     <div className="App container">
-      <Navbar collapseOnSelect>
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1}}><Link to="/">Logd</Link></div>
+        <div style={{flexBasis: '20%', alignContent: 'space-between'}}>
+        <Link to="/signup">Signup</Link>
+      <Link to="/login">Login</Link>
+      </div>
+      </div>
+      {/* <Navbar collapseOnSelect>
         <Navbar.Brand>
           <Link to="/">Logd</Link>
         </Navbar.Brand>
-    </Navbar>
-    <Routes />
+        <Navbar.Collapse>
+          <Nav className="justify-content-center">
+            <Nav.Item>
+              <Link to="/signup">Signup</Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/login">Login</Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar> */}
+      <Routes />
     </div>
   );
 
