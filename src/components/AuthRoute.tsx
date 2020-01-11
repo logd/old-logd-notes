9257a9cf5f2db81ab9c1e00b6bfed30a914fbe3b
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-01-11 14:29:04 -0500

    Add AuthRoute

---
+++ src/components/AuthRoute.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Redirect, RouteProps } from "react-router-dom";
+import { AppProps } from '../models';
+
+interface Props extends RouteProps {
+    appProps: AppProps;
+}
+
+export const AuthRoute: React.FC<Props> = ({ component: C, appProps, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        appProps.isAuth
+          ? (C ? <C {...props} {...appProps} /> : null)
+          : <Redirect
+              to={`/login?redirect=${props.location.pathname}${props.location
+                .search}`}
+            />}
+    />
+  );
+}
