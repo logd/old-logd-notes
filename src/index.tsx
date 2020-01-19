b90280da571a8b7a4fe97c3fb5fcbf7b0455c51a
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-01-19 15:55:33 -0500

    Process env fixes

--- src/index.tsx
+++ src/index.tsx
@@ -8,19 +8,38 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const stage = process.env.REACT_APP_STAGE ? process.env.REACT_APP_STAGE : 'DEV';
 
+const config: any = {
+  DEV: {
+    cognito: {
+      userPoolId: process.env.REACT_APP_USER_POOL_ID_DEV,
+      identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID_DEV,
+      userPoolWebClientId: process.env.REACT_APP_USER_POOL_APP_CLIENT_ID_DEV
+    },
+    api: {
+      endpoint: process.env.REACT_APP_API_GATEWAY_URL_DEV,
+    }
+  },
+  PROD: {
+    userPoolId: process.env.REACT_APP_USER_POOL_ID_PROD,
+    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID_PROD,
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_APP_CLIENT_ID_PROD
+  },
+  api: {
+    endpoint: process.env.REACT_APP_API_GATEWAY_URL_PROD,
+  }
+}
+
 Amplify.configure({
     Auth: {
       mandatorySignIn: true,
       region: process.env.REACT_APP_AWS_REGION,
-      userPoolId: `process.env.REACT_APP_USER_POOL_ID_${stage}`,
-      identityPoolId: `process.env.REACT_APP_IDENTITY_POOL_ID_${stage}`,
-      userPoolWebClientId: `process.env.REACT_APP_USER_POOL_APP_CLIENT_ID_${stage}`
+      ...config[stage].cognito,
     },
     API: {
       endpoints: [
         {
           name: process.env.REACT_APP_API_NAME,
-          endpoint: `process.env.REACT_APP_API_GATEWAY_URL_${stage}`,
+          endpoint: config[stage].api.endpoint,
           region: process.env.REACT_APP_AWS_REGION
         },
       ]
