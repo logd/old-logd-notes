22cc75010b567f044db3c5e9b888487e473e0f7f
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-01-19 15:26:32 -0500

    Add handling of dev vs prod stage

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,21 @@ import registerServiceWorker from "./registerServiceWorker";
 import Amplify from 'aws-amplify';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const stage = process.env.REACT_APP_STAGE ? process.env.REACT_APP_STAGE : 'DEV';
+
 Amplify.configure({
     Auth: {
       mandatorySignIn: true,
       region: process.env.REACT_APP_AWS_REGION,
-      userPoolId: process.env.REACT_APP_USER_POOL_ID,
-      identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
-      userPoolWebClientId: process.env.REACT_APP_USER_POOL_APP_CLIENT_ID
+      userPoolId: `process.env.REACT_APP_USER_POOL_ID_${stage}`,
+      identityPoolId: `process.env.REACT_APP_IDENTITY_POOL_ID_${stage}`,
+      userPoolWebClientId: `process.env.REACT_APP_USER_POOL_APP_CLIENT_ID_${stage}`
     },
     API: {
       endpoints: [
         {
           name: process.env.REACT_APP_API_NAME,
-          endpoint: process.env.REACT_APP_API_GATEWAY_URL,
+          endpoint: `process.env.REACT_APP_API_GATEWAY_URL_${stage}`,
           region: process.env.REACT_APP_AWS_REGION
         },
       ]
