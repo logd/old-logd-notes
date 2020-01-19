f4f7e9e274232c9e1f2331071d1ff0c619670f5c
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-01-19 16:07:53 -0500

    Move configs to external file

--- src/index.tsx
+++ src/index.tsx
@@ -5,41 +5,19 @@ import "./styles/styles.css";
 import registerServiceWorker from "./registerServiceWorker";
 import Amplify from 'aws-amplify';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-const stage = process.env.REACT_APP_STAGE ? process.env.REACT_APP_STAGE : 'DEV';
-
-const config: any = {
-  DEV: {
-    cognito: {
-      userPoolId: process.env.REACT_APP_USER_POOL_ID_DEV,
-      identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID_DEV,
-      userPoolWebClientId: process.env.REACT_APP_USER_POOL_APP_CLIENT_ID_DEV
-    },
-    api: {
-      endpoint: process.env.REACT_APP_API_GATEWAY_URL_DEV,
-    }
-  },
-  PROD: {
-    userPoolId: process.env.REACT_APP_USER_POOL_ID_PROD,
-    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID_PROD,
-    userPoolWebClientId: process.env.REACT_APP_USER_POOL_APP_CLIENT_ID_PROD
-  },
-  api: {
-    endpoint: process.env.REACT_APP_API_GATEWAY_URL_PROD,
-  }
-}
+import config from "./config";
 
 Amplify.configure({
     Auth: {
       mandatorySignIn: true,
       region: process.env.REACT_APP_AWS_REGION,
-      ...config[stage].cognito,
+      ...config.cognito,
     },
     API: {
       endpoints: [
         {
           name: process.env.REACT_APP_API_NAME,
-          endpoint: config[stage].api.endpoint,
+          endpoint: config.api.endpoint,
           region: process.env.REACT_APP_AWS_REGION
         },
       ]
