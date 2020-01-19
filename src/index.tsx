ccea2bec5504b6cfbda1a8ca73c3b4541fa7cfa1
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-01-19 11:17:05 -0500

    Upate Amplify config

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from "./components/App";
 import "./styles/styles.css";
 import registerServiceWorker from "./registerServiceWorker";
 import Amplify from 'aws-amplify';
-import config from './config';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 Amplify.configure({
@@ -12,23 +11,18 @@ Amplify.configure({
       mandatorySignIn: true,
       region: process.env.REACT_APP_AWS_REGION,
       userPoolId: process.env.REACT_APP_USER_POOL_ID,
-      identityPoolId: process.env.REACT_APP_USER_POOL_ID,
+      identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
       userPoolWebClientId: process.env.REACT_APP_USER_POOL_APP_CLIENT_ID
     },
-    // Storage: {
-    //   region: config.s3.REGION,
-    //   bucket: config.s3.BUCKET,
-    //   identityPoolId: config.cognito.IDENTITY_POOL_ID
-    // },
-    // API: {
-    //   endpoints: [
-    //     {
-    //       name: "notes",
-    //       endpoint: config.apiGateway.URL,
-    //       region: config.apiGateway.REGION
-    //     },
-    //   ]
-    // }
+    API: {
+      endpoints: [
+        {
+          name: process.env.REACT_APP_API_NAME,
+          endpoint: process.env.REACT_APP_API_GATEWAY_URL,
+          region: process.env.REACT_APP_AWS_REGION
+        },
+      ]
+    }
   });
 
 ReactDOM.render(
