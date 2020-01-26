2350b6b3a06632111c9cdeeef33fe6918e824b55
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-01-26 15:04:19 -0500

    Test failed and successful login

--- cypress/integration/user_login.ts
+++ cypress/integration/user_login.ts
@@ -1,25 +1,65 @@
-describe('User Login', function() {
-    it('goes to the login page', function() {
-      cy.visit('/login')
-      cy.get('[data-cy=login-page]')
+describe('User Login', function () {
+
+    describe('Failed User Login', function () {
+
+        it('goes to the login page', function() {
+            cy.visit('/login')
+            cy.get('[data-cy=login-page]')
+
+            // Clear inputs
+            cy.get('[data-cy=email-input]').clear()
+            cy.get('[data-cy=password-input]').clear()
+          })
+       
+        it('enters an unregistered email address', function () {
+            cy.get('[data-cy=email-input]')
+                .type('foo@bar.com')
+        })
+
+
+        it('enters an invalid password', function () {
+            cy.get('[data-cy=password-input]')
+                .type('bla')
+        })
+
+        it('submits the form', function () {
+            cy.get('[data-cy=login-form]').submit()
+        })
+
+        it('is not redirected', function () {
+            cy.get('[data-cy=login-page]')
+        })
     })
 
-    it('enters an email address', function() {
-        cy.get('[data-cy=email-input]')
-        .type('test1@example.com')
-      })
+    describe('Successful User Login', function() {
 
-      it('enters a password', function() {
-        cy.get('[data-cy=password-input]')
-        .type('Passw0rd!')
-      })
+        it('goes to the login page', function() {
+            cy.visit('/login')
+            cy.get('[data-cy=login-page]')
 
-      it('submits the form', function() {
-        cy.get('[data-cy=login-form]')
-        .submit()
-      })
+            // Clear inputs
+            cy.get('[data-cy=email-input]').clear()
+            cy.get('[data-cy=password-input]').clear()
+          })
+ 
+        it('enters a registered email address', function() {
+            cy.get('[data-cy=email-input]')
+            .type('test1@example.com')
+          })
 
-      it('is redirected to the homepage', function() {
-        cy.get('[data-cy=homepage]')
-      })
+          it('enters a valid password', function() {
+            cy.get('[data-cy=password-input]')
+            .type('Passw0rd!')
+          })
+
+          it('submits the form', function() {
+            cy.get('[data-cy=login-form]')
+            .submit()
+          })
+
+          it('is redirected to the homepage', function() {
+            cy.get('[data-cy=homepage]')
+          })
+    })
+
 })
