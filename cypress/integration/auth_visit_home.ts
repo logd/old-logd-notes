f10c448401506f6aac91c76fbf6255dd5dd870e2
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-02-09 13:50:38 -0500

    Use same cypress params for ci

--- cypress/integration/auth_visit_home.ts
+++ cypress/integration/auth_visit_home.ts
@@ -1,6 +1,6 @@
 describe('Authenticated acccess to homepage', function () {
     it('displays the homepage by default', function () {
-        cy.visit(`/testing-login/${Cypress.env('testEmail')}/${Cypress.env('testPassword')}`)
+        cy.visit(`/testing-login/${Cypress.env('TEST_USER_EMAIL')}/${Cypress.env('TEST_USER_PASSWORD')}`)
         cy.get('[data-cy=homepage]')
 
     })
