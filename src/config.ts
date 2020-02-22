65823572b244c0e329e0f660b7861235a06c9d85
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-02-22 16:00:44 -0500

    Add TEST to config

--- src/config.ts
+++ src/config.ts
@@ -18,10 +18,18 @@ const allConfigs: any = {
         api: {
             endpoint: process.env.REACT_APP_API_GATEWAY_URL_PROD,
         }
+    },
+    TEST: {
+        cognito: {
+            userPoolId: process.env.REACT_APP_USER_POOL_ID_DEV,
+            identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID_DEV,
+            userPoolWebClientId: process.env.REACT_APP_USER_POOL_APP_CLIENT_ID_DEV
+        },
+        api: {
+            endpoint: process.env.REACT_APP_API_GATEWAY_URL_DEV,
+        }
     }
 }
 const stage = process.env.REACT_APP_STAGE ? process.env.REACT_APP_STAGE : 'DEV';
 
-
 export default allConfigs[stage];
-
