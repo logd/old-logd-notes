179667ba59878eeb645677af186155c707759d80
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-02-22 16:00:59 -0500

    Add LoginTesting to routes

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -5,6 +5,7 @@ import { Login } from './Login';
 import { AnonRoute } from './AnonRoute';
 import { AuthRoute } from './AuthRoute';
 import { AppProps } from '../models';
+import { TestingLogin } from './TestingLogin';
 
 interface Props {
   appProps: AppProps
@@ -13,6 +14,7 @@ interface Props {
 export const Routes: React.FC<Props> = ({appProps}) => {
   return (
     <Switch>
+      <AnonRoute path="/login-testing" exact component={TestingLogin} appProps={appProps} />
       <AuthRoute path="/" exact component={Home} appProps={appProps} />
       <AnonRoute path="/login" exact component={Login} appProps={appProps} />
     </Switch>
