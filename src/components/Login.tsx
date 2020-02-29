6875356b016f670641c3dde108a17f61ebccff26
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-02-29 11:10:09 -0500

    Comment out Login and Routes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,72 +1,72 @@
-import React, { useState, useEffect } from "react";
-import { withRouter, RouteComponentProps } from "react-router-dom";
-import { Auth } from "aws-amplify";
-interface Props extends RouteComponentProps {
-  setCurrentUser: (user: any) => void;
-  currentUser: any;
-}
+// import React, { useState, useEffect } from "react";
+// import { withRouter, RouteComponentProps } from "react-router-dom";
+// import { Auth } from "aws-amplify";
+// interface Props extends RouteComponentProps {
+//   setCurrentUser: (user: any) => void;
+//   currentUser: any;
+// }
 
 
-const LoginComponent: React.FC<Props> = ({ setCurrentUser, history, currentUser }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+// const LoginComponent: React.FC<Props> = ({ setCurrentUser, history, currentUser }) => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    if (currentUser) {
-      history.push("/");
-    }
-  }, [currentUser, history])
+//   useEffect(() => {
+//     if (currentUser) {
+//       history.push("/");
+//     }
+//   }, [currentUser, history])
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
+//   function validateForm() {
+//     return email.length > 0 && password.length > 0;
+//   }
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+//   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+//     event.preventDefault();
   
-    try {
-      const user = await Auth.signIn(email, password);
-      setCurrentUser(user);
-    } catch (e) {
-      alert(e.message);
-    }
-  }
+//     try {
+//       const user = await Auth.signIn(email, password);
+//       setCurrentUser(user);
+//     } catch (e) {
+//       alert(e.message);
+//     }
+//   }
 
-  return (
-    <div data-cy="login"  style={{
-      padding: '10px 20px'
-      }}>
-      <form onSubmit={handleSubmit}>
-          <div style={{ paddingBottom: '20px'}}>
-          <label style={{ display: 'block'}}>Email</label>
-          <input
-            autoFocus
-            type="email"
-            value={email}
-            placeholder={'Email'}
-            onChange={e => setEmail(e.target.value)}
-          />
-          </div>
-          <div style={{ paddingBottom: '20px'}}>
-          <label style={{ display: 'block'}}>Password</label>
-          <input
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-            placeholder={'Password'}
-          />
-          </div>
-        <button style={{
-          WebkitAppearance: 'none',
-          color: '#212529',
-          backgroundColor: '#f8f9fa',
-          borderColor: '#f8f9fa'
-          }} disabled={!validateForm()} type="submit">
-          Sign In
-        </button>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div data-cy="login"  style={{
+//       padding: '10px 20px'
+//       }}>
+//       <form onSubmit={handleSubmit}>
+//           <div style={{ paddingBottom: '20px'}}>
+//           <label style={{ display: 'block'}}>Email</label>
+//           <input
+//             autoFocus
+//             type="email"
+//             value={email}
+//             placeholder={'Email'}
+//             onChange={e => setEmail(e.target.value)}
+//           />
+//           </div>
+//           <div style={{ paddingBottom: '20px'}}>
+//           <label style={{ display: 'block'}}>Password</label>
+//           <input
+//             value={password}
+//             onChange={e => setPassword(e.target.value)}
+//             type="password"
+//             placeholder={'Password'}
+//           />
+//           </div>
+//         <button style={{
+//           WebkitAppearance: 'none',
+//           color: '#212529',
+//           backgroundColor: '#f8f9fa',
+//           borderColor: '#f8f9fa'
+//           }} disabled={!validateForm()} type="submit">
+//           Sign In
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
 
-export const Login = withRouter(LoginComponent);
+// export const Login = withRouter(LoginComponent);
