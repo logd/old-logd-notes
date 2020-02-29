2c8c0c31c3cb3de2188169efe98634d64e8db118
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-02-29 11:24:42 -0500

    Rm auth (#12)
    * Ignore cypress envs
    
    * Move auth stuff to AuthHeader
    
    * Change script to dev
    
    * Replace app with Home
    
    * Rm gh workflow
    
    * Rm unused
    
    * Rm aws amplify
    
    * Comment out app component
    
    * Comment out Login and Routes
    
    * Comment out authheader
    
    * Rm unused

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch } from "react-router-dom";
 import Home from "./Home";
-import { Login } from './Login';
+// import { Login } from './Login';
 import { AnonRoute } from './AnonRoute';
 import { AuthRoute } from './AuthRoute';
 import { AppProps } from '../models';
@@ -14,7 +14,7 @@ export const Routes: React.FC<Props> = ({appProps}) => {
   return (
     <Switch>
       <AuthRoute path="/" exact component={Home} appProps={appProps} />
-      <AnonRoute path="/login" exact component={Login} appProps={appProps} />
+      {/* <AnonRoute path="/login" exact component={Login} appProps={appProps} /> */}
     </Switch>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { App } from "./components/App";
-import "./styles/styles.css";
 import registerServiceWorker from "./registerServiceWorker";
-import Amplify from 'aws-amplify';
-import { BrowserRouter as Router } from 'react-router-dom';
-import config from "./config";
-
-Amplify.configure({
-    Auth: {
-      mandatorySignIn: true,
-      region: process.env.REACT_APP_AWS_REGION,
-      userPoolId: config.cognito.userPoolId,
-      identityPoolId: config.cognito.identityPoolId,
-      userPoolWebClientId: config.cognito.userPoolWebClientId,
-    },
-    API: {
-      endpoints: [
-        {
-          name: process.env.REACT_APP_API_NAME,
-          endpoint: config.api.endpoint,
-          region: process.env.REACT_APP_AWS_REGION
-        },
-      ]
-    }
-  });
+import Home from './components/Home';
 
 ReactDOM.render(
-    <Router>
-    <App />
-  </Router>,
+    <Home />,
 document.getElementById("root") as HTMLElement);
 registerServiceWorker();
