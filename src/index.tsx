dd3452753a008fd3ecdab4e9e4fee6571816c25d
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-02-29 10:56:44 -0500

    Replace app with Home

--- src/index.tsx
+++ src/index.tsx
@@ -6,29 +6,28 @@ import registerServiceWorker from "./registerServiceWorker";
 import Amplify from 'aws-amplify';
 import { BrowserRouter as Router } from 'react-router-dom';
 import config from "./config";
+import Home from './components/Home';
 
-Amplify.configure({
-    Auth: {
-      mandatorySignIn: true,
-      region: process.env.REACT_APP_AWS_REGION,
-      userPoolId: config.cognito.userPoolId,
-      identityPoolId: config.cognito.identityPoolId,
-      userPoolWebClientId: config.cognito.userPoolWebClientId,
-    },
-    API: {
-      endpoints: [
-        {
-          name: process.env.REACT_APP_API_NAME,
-          endpoint: config.api.endpoint,
-          region: process.env.REACT_APP_AWS_REGION
-        },
-      ]
-    }
-  });
+// Amplify.configure({
+//     Auth: {
+//       mandatorySignIn: true,
+//       region: process.env.REACT_APP_AWS_REGION,
+//       userPoolId: config.cognito.userPoolId,
+//       identityPoolId: config.cognito.identityPoolId,
+//       userPoolWebClientId: config.cognito.userPoolWebClientId,
+//     },
+//     API: {
+//       endpoints: [
+//         {
+//           name: process.env.REACT_APP_API_NAME,
+//           endpoint: config.api.endpoint,
+//           region: process.env.REACT_APP_AWS_REGION
+//         },
+//       ]
+//     }
+//   });
 
 ReactDOM.render(
-    <Router>
-    <App />
-  </Router>,
+    <Home />,
 document.getElementById("root") as HTMLElement);
 registerServiceWorker();
