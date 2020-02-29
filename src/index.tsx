62cb0d601d5115a656b81c237d4d0ee8a430284e
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-02-29 10:58:43 -0500

    Rm unused

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { App } from "./components/App";
-import "./styles/styles.css";
 import registerServiceWorker from "./registerServiceWorker";
-import Amplify from 'aws-amplify';
-import { BrowserRouter as Router } from 'react-router-dom';
-import config from "./config";
 import Home from './components/Home';
-
-// Amplify.configure({
-//     Auth: {
-//       mandatorySignIn: true,
-//       region: process.env.REACT_APP_AWS_REGION,
-//       userPoolId: config.cognito.userPoolId,
-//       identityPoolId: config.cognito.identityPoolId,
-//       userPoolWebClientId: config.cognito.userPoolWebClientId,
-//     },
-//     API: {
-//       endpoints: [
-//         {
-//           name: process.env.REACT_APP_API_NAME,
-//           endpoint: config.api.endpoint,
-//           region: process.env.REACT_APP_AWS_REGION
-//         },
-//       ]
-//     }
-//   });
 
 ReactDOM.render(
     <Home />,
