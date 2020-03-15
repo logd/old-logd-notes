de00893e92bcd91c1da68ad845497a41a72d1681
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-03-15 16:18:30 -0400

    Index file cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import {Editor} from './components/Editor';
+import { Editor } from './components/Editor';
 
 ReactDOM.render(
     <Editor />,
