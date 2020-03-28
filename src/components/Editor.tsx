91547c5c4f27fbe54e87fb7c89d6c24771e447c1
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-03-28 15:06:09 -0400

    Replace quill with updated fork

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,28 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-
-function usePersistedState(key: string, defaultValue = "") {
-  const [state, setState] = useState<string>(
-    localStorage.getItem(key) || defaultValue
-  );
-  useEffect(() => {
-    localStorage.setItem(key, state);
-  }, [key, state]);
-  return {state, setState};
-}
+import ReactQuill from "@wyrdathru/react-quill";
+import "@wyrdathru/react-quill/dist/quill.snow.css";
+import { usePersistedState } from '../hooks';
 
 export function Editor() {
   const {state, setState} = usePersistedState("LOGD_TEXT", "");
 
-  return (
-    <div className="editor">
-    <ReactQuill
-      theme="snow"
-      value={state as string}
-      onChange={(value: string) => setState(value)}
-      placeholder={"Write something..."}
-    />
-  </div>
-  );
+  return (<ReactQuill value={state} onChange={setState} />);
 }
