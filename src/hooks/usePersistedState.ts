5a859ee761b41d0e72c47d09210fddb7ac89ab3f
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-03-28 15:12:17 -0400

    Update quill to use newer forked version (#15)
    * Replace quill with updated fork
    
    * Cleanup

---
+++ src/hooks/usePersistedState.ts
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+
+export function usePersistedState(key: string, defaultValue = {}) {
+    const [state, setState] = useState<any>(
+      localStorage.getItem(key) || defaultValue
+    );
+    useEffect(() => {
+      localStorage.setItem(key, state);
+    }, [key, state]);
+    return {state, setState};
+  }
