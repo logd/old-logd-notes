aeacbf01c724346aebce1e253ff305484023889a
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-04 14:42:46 -0400

    Add app header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export const AppHeader = () => <div>
-    <div>Logo</div>
-    <div>Login</div>
-</div>
+const Wrapper = styled.div`
+    display: flex;
+    padding: 1em;
+`;
+
+const Brand = styled.div`
+    font-size: .9em;
+    font-weight: 100;
+    color: #c9d6df;
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+`;
+
+const Main = styled.div`
+    flex: 1;
+`;
+export const AppHeader = () => <Wrapper>
+    <Main><Brand>logd</Brand></Main>
+    {/* <div>Login</div> */}
+</Wrapper>
