6007a0f77e122c855f8415354ef6df19348140dd
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-04 14:59:26 -0400

    App header (#17)
    * Add styled components
    
    * Add app header
    
    * Add header and tagline

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export const AppHeader = () => <div>
-    <div>Logo</div>
-    <div>Login</div>
-</div>
+const theme = `
+    font-size: .9em;
+    font-weight: 100;
+    color: #c9d6df;
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    padding: 1em;
+    ${theme};
+`;
+
+const Smaller = styled.div`
+    font-size: smaller;
+`;
+
+const Main = styled.div`
+    flex: 1;
+`;
+export const AppHeader = () => <Wrapper>
+    <Main>logd</Main>
+    <Smaller>Quick personal notes, saved locally only.</Smaller>
+</Wrapper>
