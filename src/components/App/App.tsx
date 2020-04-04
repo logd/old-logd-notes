89f7078e1a6e72b1ec96f2d53dd304acfc9196d9
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-04 14:10:24 -0400

    Hide header for now

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,6 @@ import { AppHeader } from '../AppHeader/AppHeader';
 import { Editor } from '../Editor/Editor';
 
 export const App = () => <div>
-    <AppHeader />
+    {/* <AppHeader /> */}
     <Editor />
 </div>
