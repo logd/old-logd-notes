5c11656bafd9c1aa37e12fad4f295dbd6ad057f6
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-04 14:49:52 -0400

    Add header and tagline

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-    display: flex;
-    padding: 1em;
-`;
-
-const Brand = styled.div`
+const theme = `
     font-size: .9em;
     font-weight: 100;
     color: #c9d6df;
     font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
 `;
 
+const Wrapper = styled.div`
+    display: flex;
+    padding: 1em;
+    ${theme};
+`;
+
+const Smaller = styled.div`
+    font-size: smaller;
+`;
+
 const Main = styled.div`
     flex: 1;
 `;
 export const AppHeader = () => <Wrapper>
-    <Main><Brand>logd</Brand></Main>
-    {/* <div>Login</div> */}
+    <Main>logd</Main>
+    <Smaller>Quick personal notes, saved locally only.</Smaller>
 </Wrapper>
