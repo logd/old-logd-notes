6f496b9ee5739c093833aa9667ef2acbcaff5be8
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-05 11:44:14 -0400

    Make privacy route exact

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,7 @@ export const App = () => <div>
     <AppHeader />
     <Switch>
       <Route path="/" exact component={Editor} />
-      <Route path="/privacy" component={Privacy} />
+      <Route path="/privacy" exact component={Privacy} />
       <Route path="*" render={() => <Redirect to={{
           pathname: "/"
       }} />} />
