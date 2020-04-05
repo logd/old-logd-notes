5f67748b6809d02f53fe447b4325ed340083fdca
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-05 12:33:12 -0400

    Full height app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,9 +3,20 @@ import { AppHeader } from '../AppHeader/AppHeader';
 import { Editor } from '../Editor/Editor';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Privacy } from '../../routes';
+import styled from 'styled-components';
 
-export const App = () => <div>
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const Main = styled.div`
+    flex: 1;
+`;
+export const App = () => <Wrapper>
     <AppHeader />
+    <Main>
     <Switch>
       <Route path="/" exact component={Editor} />
       <Route path="/privacy" exact component={Privacy} />
@@ -13,4 +24,5 @@ export const App = () => <div>
           pathname: "/"
       }} />} />
     </Switch>
-</div>
+    </Main>
+</Wrapper>
