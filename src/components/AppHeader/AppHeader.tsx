370f26584cff3af880ed733417d18e2a8e63e3d2
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-05 09:46:29 -0400

    Add login/logout

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { Link, useLocation } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const theme = `
     font-size: .9em;
     font-weight: 100;
-    color: #c9d6df;
     font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
 `;
 
@@ -25,11 +25,29 @@ const Main = styled.div`
 export const AppHeader = () => {
   const location = useLocation();
   const isHome = location.pathname === "/";
+  const { user, isAuthenticated, loginWithPopup, logout } = useContext(
+    AuthContext
+  );
+
+  const logoutWithRedirect = () =>
+    logout({
+      returnTo: window.location.origin,
+    });
 
   return (
     <Wrapper>
       <Main>{isHome ? <>logd</> : <Link to="/">logd</Link>}</Main>
       <Smaller>Quick personal notes, saved locally only.</Smaller>
+      <div>
+        {isAuthenticated ? (
+          <div>
+            user info: {user.name}{" "}
+            <button onClick={() => logoutWithRedirect()}>Log out</button>
+          </div>
+        ) : (
+          <button onClick={() => loginWithPopup()}>Login</button>
+        )}
+      </div>
     </Wrapper>
   );
 };
