9015097a87dc66c8aab5a3f7a9443506dfa47f5a
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-05 11:16:02 -0400

    Add routing for terms and privacy

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { AppHeader } from '../AppHeader/AppHeader';
 import { Editor } from '../Editor/Editor';
+import { Route, Switch } from 'react-router-dom';
+import { Privacy } from '../../routes';
+import { Terms } from '../../routes/Terms/Terms';
 
 export const App = () => <div>
     <AppHeader />
-    <Editor />
+    <Switch>
+      <Route path="/" exact component={Editor} />
+      <Route path="/privacy" component={Privacy} />
+      <Route path="/terms" component={Terms} />
+    </Switch>
 </div>
---
+++ src/routes/Terms/Terms.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Terms = () => <div>Terms</div>
