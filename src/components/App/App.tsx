a99caffffedcdac12d5b63c9e891f05a36c0865e
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-05 11:35:40 -0400

    Remove terms route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,11 @@ import { AppHeader } from '../AppHeader/AppHeader';
 import { Editor } from '../Editor/Editor';
 import { Route, Switch } from 'react-router-dom';
 import { Privacy } from '../../routes';
-import { Terms } from '../../routes/Terms/Terms';
 
 export const App = () => <div>
     <AppHeader />
     <Switch>
       <Route path="/" exact component={Editor} />
       <Route path="/privacy" component={Privacy} />
-      <Route path="/terms" component={Terms} />
     </Switch>
 </div>
