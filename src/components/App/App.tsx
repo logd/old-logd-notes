1c4ba6759fbcb6f83db60182094973848b2a0976
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-04-06 06:31:00 -0400

    Formatting fixes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import { AppHeader } from "../AppHeader/AppHeader";
 import { Editor } from "../Editor/Editor";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { Privacy } from "../../routes";
 import styled from "styled-components";
-import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -15,31 +14,24 @@ const Wrapper = styled.div`
 const Main = styled.div`
   flex: 1;
 `;
-export const App = () => {
-  const { authLoading } = useContext(AuthContext);
-  return (
-    <Wrapper>
-      <AppHeader />
-      <Main>
-        {authLoading ? (
-          <div>Loading</div>
-        ) : (
-          <Switch>
-            <Route path="/" exact component={Editor} />
-            <Route path="/privacy" exact component={Privacy} />
-            <Route
-              path="*"
-              render={() => (
-                <Redirect
-                  to={{
-                    pathname: "/",
-                  }}
-                />
-              )}
+export const App = () => (
+  <Wrapper>
+    <AppHeader />
+    <Main>
+      <Switch>
+        <Route path="/" exact component={Editor} />
+        <Route path="/privacy" exact component={Privacy} />
+        <Route
+          path="*"
+          render={() => (
+            <Redirect
+              to={{
+                pathname: "/",
+              }}
             />
-          </Switch>
-        )}
-      </Main>
-    </Wrapper>
-  );
-};
+          )}
+        />
+      </Switch>
+    </Main>
+  </Wrapper>
+);
