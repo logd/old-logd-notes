b39129c2cc54403882cd0e9d3284974f9fab709b
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-06-11 11:26:31 -0400

    Add Auth0Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,28 @@ import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import { App } from "./components/App/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Auth0Provider } from "./providers/";
+import auth0config from "./config/auth0.json";
+import history from "./utils/history";
+
+const onRedirectCallback = (appState: any) => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Auth0Provider
+      domain={auth0config.domain}
+      client_id={auth0config.clientId}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>
   </Router>,
   document.getElementById("root") as HTMLElement
 );
