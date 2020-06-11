0d16e5a65f6c57c12c2b491ac3cb81f8b78f46c6
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-06-11 12:18:56 -0400

    Prettified

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
 import ReactQuill from "@wyrdathru/react-quill";
 import "@wyrdathru/react-quill/dist/quill.snow.css";
-import { usePersistedState } from '../../hooks';
+import { usePersistedState } from "../../hooks";
 
 export function Editor() {
-  const {state, setState} = usePersistedState("LOGD_TEXT", "");
+  const { state, setState } = usePersistedState("LOGD_TEXT", "");
 
-  return (<ReactQuill value={state} onChange={setState} placeholder={"Start typing to add notes.  All content stored locally only."} />);
+  return (
+    <ReactQuill
+      value={state}
+      onChange={setState}
+      placeholder={
+        "Start typing to add notes.  All content stored locally only."
+      }
+    />
+  );
 }
