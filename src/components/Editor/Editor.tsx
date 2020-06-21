08a82335cca34c6b609e7623a02f8b89d6117be9
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-06-21 08:42:14 -0400

    Rm test code

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,44 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import ReactQuill from "@wyrdathru/react-quill";
 import "@wyrdathru/react-quill/dist/quill.snow.css";
 import { usePersistedState } from "../../hooks";
-import { Auth0Context } from "../../providers";
 
 export function Editor() {
   const { state, setState } = usePersistedState("LOGD_TEXT", "");
-  //  const { getTokenSilently } = useAuth0();
-  const { getTokenSilently } = useContext(Auth0Context);
-
-  const callApi = async () => {
-    try {
-      const token = await getTokenSilently();
-
-      const response = await fetch("http://localhost:4000/authorized", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      const responseData = await response.json();
-      console.log("responseData: ", responseData);
-
-      // setShowResult(true);
-      // setApiMessage(responseData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return (
-    <div>
-      <button onClick={callApi}>PING</button>
-      <ReactQuill
-        value={state}
-        onChange={setState}
-        placeholder={
-          "Start typing to add notes.  All content stored locally only."
-        }
-      />
-    </div>
+    <ReactQuill
+      value={state}
+      onChange={setState}
+      placeholder={
+        "Start typing to add notes.  All content stored locally only."
+      }
+    />
   );
 }
